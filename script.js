8c087387bcfdcf8af9deed116004d42acd50abb0
jenny Coranguez <lunajennykiara@gmail.com>
2022-06-22 22:39:53 -0700

    update style.css

---
+++ script.js
@@ -0,0 +1,10 @@
+var startButton = document.querySelector("#start-button");
+
+var testBtn = document.querySelector("#test-btn")
+
+
+startButton.addEventListener("click",function(){
+    console.log("Hi");
+})
+
+
